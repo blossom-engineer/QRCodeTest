8271f2be797b7b7882725a508c362b0c38e6e72a
blossom-engineer <icttechblossom@gmail.com>
2024-02-26 22:08:57 +0900

    Create Simple QRCode

---
+++ program/createQR.py
@@ -0,0 +1,20 @@
+import os
+from os.path import join, dirname, abspath
+
+import qrcode
+from PIL import Image
+
+FILE_PATH = abspath(
+    join(
+        dirname(__file__),
+        os.pardir,
+        'QR',
+        'test_qr.png'
+    )
+)
+
+# QRコード作成
+code = qrcode.make('https://github.com/blossom-engineer/QRCodeTest/tree/main')
+code.save(FILE_PATH)
+
+code.show()
